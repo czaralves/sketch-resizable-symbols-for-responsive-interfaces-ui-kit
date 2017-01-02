d1503fbb622320b8856717f86141de076e2e51b7
Cesar Alves <Cesar@mac7950.local>
2017-01-02 17:04:32 -0200

    commit

---
+++ SketchResizableSymbolsforResponsiveInterfacesUIKit/APDesktopHdDetachedSymbolsContentViaCraftVC.h
@@ -0,0 +1,39 @@
+//
+//  APDesktopHdDetachedSymbolsContentViaCraftVC.h
+//  SketchResizableSymbolsforResponsiveInterfaces
+//
+//  Created by czaralves@gmail.com on 02/01/2017, using AnimaApp.com, under MIT license.
+//  Copyright © 2017 Company Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ANViewController.h"
+#import "ANLabel.h"
+
+@interface APDesktopHdDetachedSymbolsContentViaCraftVC : ANViewController
+
+@property (weak, nonatomic, nullable) IBOutlet UIView *group2;
+@property (weak, nonatomic, nullable) IBOutlet UIImageView *group2Group2;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlg;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgTopNav;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgTopNavTopNav;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgTopNavGroup;
+@property (weak, nonatomic, nullable) IBOutlet UIImageView *commonlgTopNavGroupRectangle;
+@property (weak, nonatomic, nullable) IBOutlet UILabel *commonlgTopNavGroupLabelFrancesco;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgLogo;
+@property (weak, nonatomic, nullable) IBOutlet UIImageView *commonlgLogoFreeWilling;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgNavItems;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgNavItemsCurrent;
+@property (weak, nonatomic, nullable) IBOutlet UILabel *commonlgNavItemsLabelMembers;
+@property (weak, nonatomic, nullable) IBOutlet UILabel *commonlgNavItemsLabelJobs;
+@property (weak, nonatomic, nullable) IBOutlet UILabel *commonlgNavItemsLabelInspiration;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgFooter;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgFooterFooterArea;
+@property (weak, nonatomic, nullable) IBOutlet UIView *commonlgFooterLinks;
+@property (weak, nonatomic, nullable) IBOutlet UILabel *commonlgFooterLinksLabelFooterLinks;
+@property (weak, nonatomic, nullable) IBOutlet UILabel *commonlgFooterLinksLabel1;
+@property (weak, nonatomic, nullable) IBOutlet UILabel *commonlgFooterLinksLabel2;
+
+
+
+@end
---
+++ SketchResizableSymbolsforResponsiveInterfacesUIKit/APDesktopHdDetachedSymbolsContentViaCraftVC.m
@@ -0,0 +1,14 @@
+//
+//  APDesktopHdDetachedSymbolsContentViaCraftVC.m
+//  SketchResizableSymbolsforResponsiveInterfaces
+//
+//  Created by czaralves@gmail.com on 02/01/2017, using AnimaApp.com, under MIT license.
+//  Copyright © 2017 Company Name. All rights reserved.
+//
+
+#import "APDesktopHdDetachedSymbolsContentViaCraftVC.h"
+#import "AnimaSDK.h"
+
+@implementation APDesktopHdDetachedSymbolsContentViaCraftVC
+
+@end
